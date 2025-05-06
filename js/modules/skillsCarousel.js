31fe165150a30b30979927fe3b78bd256d529cca
Alexandros Smponias <MLP3FE@bosch.com>
2025-05-06 09:48:53 +0200

    Fixed Technical Skills

--- js/modules/skillsCarousel.js
+++ js/modules/skillsCarousel.js
@@ -11,7 +11,11 @@ export function initSkillsCarousel() {
   const itemWidth = skillCategories[0]?.offsetWidth + parseInt(getComputedStyle(skillCategories[0]).marginRight) || 0;
 
   function updateCarousel() {
-    carousel.scrollTo({ left: currentIndex * itemWidth, behavior: 'smooth' });
+    skillCategories[currentIndex].scrollIntoView({
+      behavior: 'smooth',
+      inline: 'center',
+      block: 'nearest'
+    });
     indicators.forEach((ind, idx) => ind.classList.toggle('active', idx === currentIndex));
   }
 
